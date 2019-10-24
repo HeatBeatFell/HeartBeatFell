464d0311f10ebce447d358e788b14938eae4f759
ZhangFeQiang <xxxxx@qq.com>
2019-10-24 21:13:27 +0800

    Add structure

---
+++ day_01.review-vue/supervisory/src/views/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-card class="box-card" shadow="always">
+    <el-form :model="loginForm" :rules="loginrules" ref="loginref" label-width="100px">
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="loginForm.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="loginForm.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" round>主要按钮</el-button>
+        <el-button type="info" round>信息按钮</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: ""
+      },
+      loginrules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 10, message: "长度在 6 到 10 个字符", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {}
+};
+</script>
+<style>
+.box-card {
+  margin: auto;
+  width: 480px;
+}
+</style>
---
+++ day_01.review-vue/supervisory/src/views/home.vue
@@ -0,0 +1,58 @@
+<template>
+  <el-container>
+    <el-header>
+      <div class="logo">
+        <img src="../assets/timg.jpg" alt />
+        <span>传智健康档案管理系统</span>
+      </div>
+      <el-button type="info">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style>
+.logo {
+  display: flex;
+  font-size: 22px;
+  align-items: center;
+  user-select: none;
+}
+img {
+  padding: 0;
+  margin: 0;
+  font-size: 14px;
+  width: 60px;
+  margin-right: 10px;
+}
+
+.el-header {
+  background-color: #ffffff;
+  color: #333;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+</style>
